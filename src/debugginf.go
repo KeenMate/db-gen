c48ba1c4e2038b41434d407e4f7301bb1b860498
Jan Rada <31016933+ZelvaMan@users.noreply.github.com>
2023-12-27 10:52:16 +0100

    fix permissions of created temp files

--- src/debugginf.go
+++ src/debugginf.go
@@ -10,19 +10,19 @@ import (
 const tempFolder = "C:\\tmp\\db-gen"
 
 // Saves functions to temp file on disk for development and debugging
-func SaveToTempFile(functions interface{}, fileNamePrefix string) (err error) {
+func SaveToTempFile(content interface{}, fileNamePrefix string) (err error) {
 	err = os.MkdirAll(tempFolder, 777)
 	if err != nil {
 		return
 	}
 
-	saveJson, err := json.MarshalIndent(functions, "", "  ")
+	saveJson, err := json.MarshalIndent(content, "", "  ")
 	if err != nil {
 		return
 	}
 
 	filename := filepath.Join(tempFolder, time.Now().Format("2006-01-02-15-04-05")+"-"+fileNamePrefix+".json")
-	err = os.WriteFile(filename, saveJson, 777)
+	err = os.WriteFile(filename, saveJson, 0777)
 	if err != nil {
 		return
 	}
